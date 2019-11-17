882e5087bfa0e998d339b67556511c60d96f2c17
DESKTOP-F2URIQD\alceo <giovanni.zorzetti@gmail.com>
2019-11-17 10:50:38 +0100

    Aggiunto commenti

--- app/src/main/java/com/example/listview/MainActivity.java
+++ app/src/main/java/com/example/listview/MainActivity.java
@@ -11,6 +11,36 @@ import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Recuperiamo il riferimento all'oggetto ListView
+        ListView listView = (ListView) findViewById(R.id.list_view);
+
+        // Recupera l'array di stringhe
+        String[] planets = getResources().getStringArray(R.array.sistema_solare);
+
+        // Crea un oggetto arrayadapter usando l'elenco delle stringhe
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.simple_list_item_1, planets);
+
+        // Assegna l'adapter alla listView
+        listView.setAdapter(adapter);
+
+        // Assegna evebntule click
+        listView.setOnItemClickListener(mioClick);
+
+        // Assegnao il long click
+        listView.setOnItemLongClickListener(clickProlungato);
+
+
+    }
+
+    /**
+     * Implementazione interfaccia per il click
+     *
+     */
     private AdapterView.OnItemClickListener mioClick = new AdapterView.OnItemClickListener() {
         @Override
         public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
@@ -19,34 +49,17 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    /**
+     * Implementazione interfaccia per il click prolungato
+     *
+     */
     private AdapterView.OnItemLongClickListener clickProlungato = new AdapterView.OnItemLongClickListener() {
         @Override
         public boolean onItemLongClick(AdapterView<?> adapterView, View view, int position, long id) {
             Log.d("MIO", "Click prolungato su " + position);
+            view.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
             return false;
         }
     };
 
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
-        // Recuperiamo il riferimento all'oggetto ListView
-        ListView listView = (ListView) findViewById(R.id.list_view);
-        // Recupera l'array di stringhe
-        String[] planets = getResources().getStringArray(R.array.sistema_solare);
-        // Crea un oggetto arrayadapter
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.simple_list_item_1, planets);
-        // Assegna l'adapter
-        listView.setAdapter(adapter);
-
-        listView.setOnItemClickListener(mioClick);
-
-        listView.setOnItemLongClickListener(clickProlungato);
-
-
-    }
 }
